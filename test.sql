c48cc2631449c5c552f93811ff8ff435cf457b20
hoooooooooori <hori562208@gmail.com>
2024-09-18 15:26:06 +0900

    残高変更機能の実装

--- test.sql
+++ test.sql
@@ -3,6 +3,8 @@ USE appBank;
 
 -- 既存のテーブルを削除する場合
 DROP TABLE IF EXISTS account_info;
+DROP TABLE IF EXISTS remittance_log;
+DROP TABLE IF EXISTS claim_log;
 
 CREATE TABLE account_info (
    account_num VARCHAR(10) NOT NULL,
@@ -11,8 +13,29 @@ CREATE TABLE account_info (
    balance DECIMAL(10, 2) NOT NULL,
    PRIMARY KEY (account_num)
 );
+-- 送金ログ
+CREATE TABLE remittance_log (
+   id INT(10) NOT NULL AUTO_INCREMENT,
+   sender VARCHAR(10000) NOT NULL,
+   destination VARCHAR(100) NOT NULL,
+   amount DECIMAL(10, 2) NOT NULL,
+   msg VARCHAR(1000),
+   dateinfo DATETIME NOT NULL,
+   flag BOOLEAN NOT NULL,
+   PRIMARY KEY (id)
+);
 
-
+-- 請求ログ
+CREATE TABLE claim_log (
+   id INT(10) NOT NULL  AUTO_INCREMENT,
+   sender VARCHAR(10000) NOT NULL,
+   destination VARCHAR(100) NOT NULL,
+   amount DECIMAL(10, 2) NOT NULL,
+   msg VARCHAR(1000),
+   dateinfo DATETIME NOT NULL,
+   flag BOOLEAN NOT NULL,
+   PRIMARY KEY (id)
+);
 
 -- データの挿入
 INSERT INTO account_info (account_num, user_name, image_path,balance) VALUES
@@ -23,6 +46,13 @@ INSERT INTO account_info (account_num, user_name, image_path,balance) VALUES
 ('567890', 'wada riki','human5.png', 3000.00),
 ('678901', 'suzuki tarou','human6.png', 20000.00);
 
+INSERT INTO remittance_log (sender, destination, amount, msg, dateinfo, flag) VALUES
+('123456', '234567', 500.00, "hello", '2024-09-18 14:30:00', TRUE);
+
+INSERT INTO claim_log (sender, destination, amount, msg, dateinfo, flag) VALUES
+('345678', '456789', 1500.00, "thankyou", '2024-09-18 14:30:00', TRUE);
+
 -- データの確認
 SELECT * FROM account_info;
-
+SELECT * FROM remittance_log;
+SELECT * FROM claim_log;
